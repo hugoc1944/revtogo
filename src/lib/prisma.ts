7798f8b627946a307c768e5271f8e0bf2324f8ea
hugoc1944 <125464948+hugoc1944@users.noreply.github.com>
2026-02-04 17:11:50 +0000

    Prisma fix for persistent deploy error

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,14 +1,16 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
 const globalForPrisma = globalThis as unknown as {
-  prisma?: PrismaClient;
+  prisma?: ReturnType<typeof createPrismaClient>;
 };
 
+function createPrismaClient() {
+  return new PrismaClient().$extends(withAccelerate());
+}
+
 export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log: ["error"],
-  });
+  globalForPrisma.prisma ?? createPrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
