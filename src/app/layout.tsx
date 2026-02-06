db903e0be1a420a156ae111253fd2aecd7db8569
hugoc1944 <125464948+hugoc1944@users.noreply.github.com>
2026-02-06 14:37:21 +0000

    Added GTM tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,12 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import type { Metadata } from "next";
+import Script from "next/script";
 import { DesignRequestOverlay } from "@/components/design-request/design-request-overlay";
 
 const inter = Inter({
   subsets: ["latin"],
-  weight: ["300" ,"400", "500", "600"],
+  weight: ["300", "400", "500", "600"],
   variable: "--font-inter",
   display: "swap",
 });
@@ -26,9 +27,38 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt">
+      <head>
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-ND74VLKD');
+            `,
+          }}
+        />
+        {/* End Google Tag Manager */}
+      </head>
+
       <body className={`${inter.variable} bg-bg text-ink antialiased`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-ND74VLKD"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         {children}
-         <DesignRequestOverlay />
+        <DesignRequestOverlay />
       </body>
     </html>
   );
